2d2678c05555bdf29d28e6ba7a8adc92a2f6cc65
Nikolay Selyutin <nikolay.selyutin@lamie.io>
2025-09-13 23:15:36 +0200

    Init solution structure

---
+++ src/packages/Currency.Reference.Iso4217/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Currency.Reference.Iso4217;
+
+public class Class1
+{
+
+}
---
+++ src/tests/Currency.Reference.Iso4217.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Currency.Reference.Iso4217.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
