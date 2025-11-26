ff6f665cc68e4104c44f3ba78d21b28e9221361a
Nikolay Selyutin <nikolay.selyutin@lamie.io>
2025-11-26 15:53:05 +0100

    refactor: Improve source generators

--- src/packages/Currency.Reference.Iso4217.Generators/Constants.cs
+++ src/packages/Currency.Reference.Iso4217.Generators/Constants.cs
@@ -4,6 +4,6 @@ internal static class Constants
 {
     public const string DefaultNamespace = "Currency.Reference.Iso4217";
     public const string DiagnosticsTitle = "Generator error";
-    public const string ErrorMark = "__ERROR__";
+    public const string ErrorMark = "#ERROR:";
     public const string GeneratorName = "Currency.Reference.Iso4217.Generators source generator";
 }
